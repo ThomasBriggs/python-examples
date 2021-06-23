408e1bc686d15022f540706705feecf755fc11b7
Thomas Briggs <thomas.briggs333@gmail.com>
2021-06-23 23:29:45 +0100

    Made node a private inner class

--- BinaryTree/binTree.py
+++ BinaryTree/binTree.py
@@ -1,11 +1,10 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-
 class Tree:
+    class __Node:
+        def __init__(self, val):
+            self.val = val
+            self.left = None
+            self.right = None
+
     def __init__(self, data=[]):
         self.root = None
         if data is not []:
@@ -14,19 +13,19 @@ class Tree:
 
     def add(self, val):
         if self.root is None:
-            self.root = Node(val)
+            self.root = self.__Node(val)
         else:
             self.addRec(val, self.root)
 
     def addRec(self, val, node):
         if val < node.val:
             if node.left is None:
-                node.left = Node(val)
+                node.left = self.__Node(val)
             else:
                 self.addRec(val, node.left)
         else:
             if node.right is None:
-                node.right = Node(val)
+                node.right = self.__Node(val)
             else:
                 self.addRec(val, node.right)
 
@@ -71,3 +70,5 @@ class Tree:
         else:
             print("Node not found!")
 
+tree = Tree([5,3,7,8,9,1,3,4,5])
+tree.printTree()
